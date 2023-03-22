0a3639c8bb1f1471bab1e088a8750557557ce320
nktkr <ankit.12th.2018@gmail.com>
2023-03-22 18:13:19 +0530

    solved 38th problem

--- 4-Numbers/38-Fibonacci-series.cpp
+++ 4-Numbers/38-Fibonacci-series.cpp
@@ -1,17 +1,72 @@
-// Fibonacci Series 
+// Fibonacci Series
 
 // i. Print Fibonacci Series up to n number of terms
 
-#include<iostream>
+// #include <iostream>
+// using namespace std;
+
+// void PrintFibonacciton(int n)
+// {
+//   int temp, pre = 0, next = 1;
+
+//   if (n >= 1)
+//     cout << pre << " ";
+//   if (n >= 2)
+//     cout << next << " ";
+  
+//   for(int i = 1; i <= n - 2; i++)
+//   {
+//     temp = next + pre;
+//     cout << temp << " ";
+//     pre = next;
+//     next = temp;    
+//   } 
+// }
+
+// int main()
+// {
+//   int n;
+//   cout << "Enter the number of terms you want get: ";
+//   cin >> n;
+
+//   PrintFibonacciton(n);
+//   return 0;
+// }
+
+
+
+// ii. Program to Generate Sequence up to a certain number
+
+#include <iostream>
 using namespace std;
 
 void PrintFibonacciton(int n)
+{
+  int temp, pre = 0, next = 1;
+
+  if (n >= 0)
+    cout << pre << " ";
+  if (n >= 1)
+    cout << next << " ";
+  
+  while(true)
+  {
+    temp = next + pre;
+    if(temp >= n)
+      break;
+    cout << temp << " ";
+    pre = next;
+    next = temp;    
+  } 
+  cout << endl;
+}
+
 int main()
 {
   int n;
-  cout << "Enter the number of terms you want get: ";
+  cout << "Enter the certain number till which you want to generate fibonacci series: ";
   cin >> n;
 
   PrintFibonacciton(n);
-  return 0;  
+  return 0;
 }
---
+++ 4-Numbers/tempCodeRunnerFile.cpp
@@ -0,0 +1,2 @@
+    if (temp >= n)
+      break;
